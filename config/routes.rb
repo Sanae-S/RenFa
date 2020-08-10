140a035b392d76ea937864284e1ffeff6f13ad19
vagrant <vagrant@localhost.localdomain>
2020-08-10 12:19:29 +0000

    [Fix]ヘッダーのパス修正

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     get "signup", :to => "users/registrations#new"
     get "login", :to => "users/sessions#new"
     get "logout", :to => "users/sessions#destroy"
-end
-
+  end
+resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
