017c5922011789608bf21741deee04301b75389c
vagrant <vagrant@localhost.localdomain>
2020-08-13 22:48:42 +0000

    [Finish] 画像投稿（１枚のみなら）

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,9 +1,14 @@
 class PostImagesController < ApplicationController
 
-	def new
+  def new
+  	@post_image = PostImage.new
   end
 
   def create
+  	@post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
   end
 
   def index
@@ -20,5 +25,10 @@ class PostImagesController < ApplicationController
 
   def destroy
   end
-  
+private
+  def post_image_params
+    params.require(:post_image).permit(:animal_name, :image, :introduction)
+  end
+
 end
+#複数画像をアップするために配列にする。, :images[]
