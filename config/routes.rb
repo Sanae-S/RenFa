191389683cab7650db5f7c232ac145d6a9f905ef
vagrant <vagrant@localhost.localdomain>
2020-08-13 19:31:02 +0000

    [Add]PostImageModel/Controller/routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ resources :users
 put "/users/:id/hide" => "users#hide", as: 'users_hide'
 
 root 'post_images#index'
+resources :post_images, only: [:new, :create, :index, :show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
