d97971fb46686eb998b9695f6f6f0f316c033db2
vagrant <vagrant@localhost.localdomain>
2020-08-26 14:49:44 +0000

    [Update]いいね非同期通信、サイドバー修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,20 @@
 class FavoritesController < ApplicationController
-
+  before_action :set_post_image
   def create
   	post_image = PostImage.find(params[:post_image_id])
     favorite = current_user.favorites.new(post_image_id: post_image.id)
     favorite.save
-    redirect_to post_image_path(post_image)
   end
 
   def destroy
   	post_image = PostImage.find(params[:post_image_id])
     favorite = current_user.favorites.find_by(post_image_id: post_image.id)
     favorite.destroy
-    redirect_to post_image_path(post_image)
+  end
+
+private
+  def set_post_image
+   @post_image = PostImage.find(params[:post_image_id])
   end
 
 end
