6083f5c30702d489832bf1d0bafa767f3317642a
vagrant <vagrant@localhost.localdomain>
2020-08-27 11:55:13 +0000

    [fix]schema

--- db/migrate/20200822092215_create_entries.rb
+++ db/migrate/20200822092215_create_entries.rb
@@ -2,8 +2,8 @@ class CreateEntries < ActiveRecord::Migration[5.2]
   def change
     create_table :entries do |t|
 
-      t.references :user, foreign_key: true
-      t.references :room, foreign_key: true
+      t.integer :user_id, foreign_key: true
+      t.integer :room_id, foreign_key: true
       t.timestamps
     end
   end
