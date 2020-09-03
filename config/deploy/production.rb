3651894893a1fc33aab5a524cf3a9b5a41da7701
vagrant <vagrant@localhost.localdomain>
2020-09-03 11:07:47 +0000

    [Update]config/deploy/production.rbを編集

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
+server '54.178.82.24', user: 'ec2-user', roles: %w{app web db}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
